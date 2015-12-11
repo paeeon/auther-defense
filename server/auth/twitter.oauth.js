231c42cb4d3087655d77b041259fa7f722968db1
Lily Jen <lily@lilyjen.com>
2015-12-11 15:52:31 -0500

    Adds Lily's Twitter API keys

--- server/auth/twitter.oauth.js
+++ server/auth/twitter.oauth.js
@@ -14,8 +14,8 @@ router.get('/callback', passport.authenticate('twitter', {
 }));
 
 passport.use(new TwitterStrategy({
-	consumerKey: 'xe86sGm0HUu7qTwnQBq89dX02',
-	consumerSecret: 'pD1XEQgYppFpztd86H14d8EQWR0627yZRrFMP53DSOuMf4YaoQ',
+	consumerKey: 'Zz5h6HKv7KLqB7oDM9Sf1u6Nz',
+	consumerSecret: 'kXv45GZsFyZcGUWiWbIm7l8We3PWhyzqVaQgTc1ZWVPkFh5gj2',
 	callbackURL: 'http://127.0.0.1:8080/auth/twitter/callback'
 }, function (token, refreshToken, profile, done) { 
 	User.findOne({'twitter.id': profile.id }, function (err, user) {
